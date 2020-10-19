ed5789a2179b54519c130a0906ae8aef38acd427
ElGanzoConLaRinonera <43580400+ElGanzoConLaRinonera@users.noreply.github.com>
2020-10-19 11:58:51 +0200

    Initial commit

---
+++ src/main/java/cz/fim/uhk/insertions/InsertionsApplication.java
@@ -0,0 +1,13 @@
+package cz.fim.uhk.insertions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsertionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsertionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/fim/uhk/insertions/InsertionsApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.fim.uhk.insertions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsertionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
